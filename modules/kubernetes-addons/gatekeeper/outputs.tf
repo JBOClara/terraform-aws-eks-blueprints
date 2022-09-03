5643ea7f1a99e8ce5e2baf35a513af580f5e826b
jboulanger <julien.boulanger@fr.clara.net>
2022-09-03 18:22:39 +0200

    feat(helm): install gatekeeper from eks blueprint

--- modules/kubernetes-addons/gatekeeper/outputs.tf
+++ modules/kubernetes-addons/gatekeeper/outputs.tf
@@ -2,6 +2,5 @@ output "argocd_gitops_config" {
   description = "Configuration used for managing the add-on with ArgoCD"
   value = var.manage_via_gitops ? {
     enable             = true
-    serviceAccountName = local.service_account
   } : null
 }
